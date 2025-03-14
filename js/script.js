69ad171b69c73fb591b0e6e0ea182f344e8d7d00
Foysal-Munsy <bdfoysal555@gmail.com>
2025-03-15 03:31:25 +0600

    btn works

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+function validateForm(event) {
+  event.preventDefault();
+
+  const user = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (!user) {
+    alert("Please enter a username.");
+    return;
+  }
+
+  if (password === "123456") {
+    document.getElementById("banner").classList.add("hidden");
+    document.getElementById("nv").classList.remove("hidden");
+  } else {
+    alert("Incorrect password. Please enter 123456.");
+  }
+}
+function logout() {
+  document.getElementById("banner").classList.remove("hidden");
+  document.getElementById("nv").classList.add("hidden");
+}
