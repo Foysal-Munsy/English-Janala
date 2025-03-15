3dfc36b31f6cb9be5350218eacba579ba6f67174
Foysal-Munsy <bdfoysal555@gmail.com>
2025-03-15 15:49:46 +0600

    btn added dynamically

--- js/script.js
+++ js/script.js
@@ -1,20 +1,43 @@
-function login(event) {
-  event.preventDefault();
-  const user = document.getElementById("username").value;
-  //   console.log(user);
-  const password = document.getElementById("password").value;
-  if (!user) {
-    alert("Please enter a username.");
-    return;
+// function login(event) {
+//   event.preventDefault();
+//   const user = document.getElementById("username").value;
+//   //   console.log(user);
+//   const password = document.getElementById("password").value;
+//   if (!user) {
+//     alert("Please enter a username.");
+//     return;
+//   }
+//   if (password === "123456") {
+//     document.getElementById("banner").classList.add("hidden");
+//     document.getElementById("nv").classList.remove("hidden");
+//   } else {
+//     alert("Incorrect password. Please enter 123456.");
+//   }
+// }
+// function logout() {
+//   document.getElementById("banner").classList.remove("hidden");
+//   document.getElementById("nv").classList.add("hidden");
+// }
+
+const vocabulariesBtn = () => {
+  const url = "https://openapi.programming-hero.com/api/levels/all";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevel(data));
+};
+const displayLevel = (levels) => {
+  //   console.log(levels.data[0].level_no);
+  const vocabulariesBtnContainer = document.getElementById(
+    "vocabularies-btn-container"
+  );
+  for (let i = 0; i < levels.data.length; i++) {
+    // console.log(levels.data[i].level_no);
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson- ${levels.data[i].level_no}</button>
+    `;
+    vocabulariesBtnContainer.append(div);
   }
-  if (password === "123456") {
-    document.getElementById("banner").classList.add("hidden");
-    document.getElementById("nv").classList.remove("hidden");
-  } else {
-    alert("Incorrect password. Please enter 123456.");
-  }
-}
-function logout() {
-  document.getElementById("banner").classList.remove("hidden");
-  document.getElementById("nv").classList.add("hidden");
-}
+};
+
+vocabulariesBtn();
